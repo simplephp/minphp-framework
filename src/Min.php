000ec676b5fd99ec87e83c95e6e973699afc1b4f
kevin <841694874@qq.com>
2019-02-28 17:18:49 +0800

    基础版本

---
+++ src/Min.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * description
+ * @author         kevin <askyiwang@gmail.com>
+ * @date           2019/2/28
+ * @since          1.0
+ */
+class Min
+{
+
+    /**
+     * version
+     */
+    const VERSION = '1.0.0';
+
+    /**
+     * @var $_app
+     */
+    protected static $_app;
+
+    /**
+     * @var $container
+     */
+    public static $container;
+}
---
+++ src/console/Application.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace min\console;
+
+/**
+ * App类
+ * @author 刘健 <coder.liu@qq.com>
+ */
+class Application extends \min\base\Application
+{
+
+    // 命令命名空间
+    public $commandNamespace = '';
+
+    // 命令
+    public $commands = [];
+
+    // 执行功能 (CLI模式)
+    public function run()
+    {
+        if (PHP_SAPI != 'cli') {
+            throw new \RuntimeException('Please run in CLI mode.');
+        }
+
+    }
+}
