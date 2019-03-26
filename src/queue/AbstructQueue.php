7841b0a3c95fd03db5e7ecba779a3f47188c45a0
kevin <841694874@qq.com>
2019-03-26 18:50:48 +0800

    first

--- src/queue/AbstructQueue.php
+++ src/queue/AbstructQueue.php
@@ -10,6 +10,27 @@ namespace min\queue;
 
 abstract class AbstructQueue implements QueueInterface
 {
+
+    /**
+     * 定义一个用来序列化对象的函数
+     * @param $obj
+     * @return string
+     */
+    public static function serialize(mixed $data)
+    {
+        return base64_encode(gzcompress(serialize($data)));
+    }
+
+    /**
+     * 反序列化
+     * @param string $text
+     * @return mixed
+     */
+    public static function unserialize(string $text)
+    {
+        return unserialize(gzuncompress(base64_decode($text)));
+    }
+
     /**
      * 连接消息队列
      * @return mixed
